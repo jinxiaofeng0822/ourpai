335f2b78bfb46b50ac4f766edbc14395ca44e381
jinxf0822 <jinxf0822@foxmail.com>
2019-04-25 00:36:43 +0800

    保存文件名字

--- src/main/java/com/jxf/ourpai/controller/movieNameController.java
+++ src/main/java/com/jxf/ourpai/controller/movieNameController.java
@@ -8,6 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @author Jin Xiaofeng
  */
@@ -19,8 +24,8 @@ public class movieNameController {
     private MovieNameMapper mapper;
 
     @RequestMapping("/addName")
-    public String addName(){
-        MovieName mn=new MovieName();
+    public String addName() {
+        MovieName mn = new MovieName();
         mn.setMovieName("电影名字");
         mn.setFullpathName("路径123123");
         mapper.insertSelective(mn);
@@ -28,6 +33,66 @@ public class movieNameController {
     }
 
     public static void main(String[] args) {
+        int count = 0;
+        File file = new File("C:\\Users\\小风\\Desktop\\testfile");
+        System.out.println(file.exists());
 
+        Queue<File> queue = new ArrayDeque<>();//保存目录
+        Stack<File> stack = new Stack<>();
+
+        //这里的栈，类似深度优先搜索
+        File[] files = file.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isFile()) {
+                count++;
+                System.out.println(files[i].getPath());
+            } else {
+                stack.push(files[i]);
+            }
+        }
+
+        while (stack.size() != 0) {
+            File pf = stack.pop();
+            File[] pfs = pf.listFiles();
+            for (int i = 0; i < pfs.length; i++) {
+                if (pfs[i].isFile()) {
+                    count++;
+                    System.out.println(pfs[i].getPath());
+                } else {
+                    stack.push(pfs[i]);
+                }
+            }
+        }
+        System.out.println("执行完毕");
+        System.out.println("共有" + count + "个文件");
+
+        System.out.println("开始队列打印");
+        file.listFiles();
+        count=0;
+        for (int i = 0; i < files.length; i++) {
+            if(files[i].isFile()){
+                count++;
+                System.out.println(files[i].getPath());
+            }else{
+                queue.add(files[i]);
+            }
+        }
+
+
+        while (queue.size()!=0){
+            File pf = queue.poll();
+            File[] pfs = pf.listFiles();
+            for (int i = 0; i < pfs.length; i++) {
+                if(pfs[i].isFile()){
+                    count++;
+                    System.out.println(pfs[i].getPath());
+                }else{
+                    queue.add(pfs[i]);
+                }
+            }
+        }
+
+        System.out.println("执行完毕");
+        System.out.println("共有" + count + "个文件");
     }
 }
